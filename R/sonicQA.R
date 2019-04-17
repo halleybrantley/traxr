c5a8a3ff66f6de9ca4b3c5e577bb9980225169b3
Halley Brantley <halleybrantley@gmail.com>
2019-04-17 10:09:23 -0400

    added splines::

--- R/sonicQA.R
+++ R/sonicQA.R
@@ -15,7 +15,7 @@ sonicQA <- function(spod, nodes){
     }
     spod[, paste0(node, ".Sonic.Temp..oC.")] <-
       zoo::na.locf( spod[,paste0(node, ".Sonic.Temp..oC.")])
-    formula <- as.formula(paste0(node, ".Sonic.Temp..oC.~ns(time,84)"))
+    formula <- as.formula(paste0(node, ".Sonic.Temp..oC.~splines::ns(time,84)"))
     preds <- predict(lm(formula, spod))
     resid <- resid(lm(formula, spod))
     miss <- which(resid < -2.5)
